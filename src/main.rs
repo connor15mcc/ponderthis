300f92394b6a9abca6218ebdbb71caedc971b3e4
Connor McCarthy <connor15mcc@gmail.com>
2023-08-21 10:19:07 -0400

    add observed pattern for calculating terms

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use num_traits::pow;
 use std::cmp::max;
 
 fn main() {
+    let mut dp: Vec<BigUint> = vec![BigUint::from(1_u32), BigUint::from(36_u32)];
+    let upper_search_bound: BigUint = pow(BigUint::from(10_u32), 100);
+
+    while dp.last().unwrap() <= &upper_search_bound {
+        let n = dp.len();
+        dp.push((dp.get(n - 1).unwrap() - BigUint::from(1_u32)).pow(2) / dp.get(n - 2).unwrap());
+        println!("{}", dp.last().unwrap());
+    }
+
+    dp.retain(|term| term.lt(&upper_search_bound));
+    println!(
+        "{} square triangular numbers less than {:#?}",
+        dp.len(),
+        upper_search_bound
+    );
+
+    // ping_pong_search(dp);
+}
+
+fn square_it(n: &BigUint) -> BigUint {
+    pow(n.to_owned(), 2)
+}
+
+fn triangular_it(m: &BigUint) -> BigUint {
+    (m * (m + 1_u32)) / 2_u32
+}
+
+fn ping_pong_search(dp: Vec<BigUint>) {
     let upper_search_bound: BigUint = pow(BigUint::from(10_u32), 20);
     let mut highest: BigUint = 0_u32.into();
 
@@ -11,7 +39,7 @@ fn main() {
     let mut num_square_triangular = 0;
 
     while highest <= upper_search_bound {
-        println!("Searching n={n}, m={m}");
+        // println!("Searching n={n}, m={m}");
         let square = square_it(&n);
         let triangular = triangular_it(&m);
 
@@ -26,7 +54,10 @@ fn main() {
             }
             std::cmp::Ordering::Equal => {
                 num_square_triangular += 1;
-                println!("Found square triangular (#{num_square_triangular})");
+                println!(
+                    "Found square triangular (#{num_square_triangular}): {square}, (n={n}, m={m})"
+                );
+                // assert!(&square == dp.get(num_square_triangular - 1).unwrap());
                 highest = max(highest, square);
                 n += 1_u32;
                 m += 1_u32;
@@ -35,12 +66,4 @@ fn main() {
     }
 
     println!("Answer: {num_square_triangular}")
-}
-
-fn square_it(n: &BigUint) -> BigUint {
-    pow(n.to_owned(), 2)
-}
-
-fn triangular_it(m: &BigUint) -> BigUint {
-    (m * (m + 1_u32)) / 2_u32
 }
